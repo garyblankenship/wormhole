c16528dd6ce60611e7e0088d9a3f0be12ef5be29
Gary Blankenship <gary@blankenship.me>
2026-07-19 00:33:02 -0400

    refactor: remove unused request pools

--- pools.go
+++ pools.go
@@ -6,72 +6,12 @@ import (
 	"github.com/garyblankenship/wormhole/v2/types"
 )
 
-// Memory pools for frequently allocated objects
-var (
-	textRequestPool = sync.Pool{
-		New: func() any {
-			return &types.TextRequest{
-				Messages: make([]types.Message, 0, 4), // Pre-allocate capacity for 4 messages
-			}
-		},
-	}
-
-	structuredRequestPool = sync.Pool{
-		New: func() any {
-			return &types.StructuredRequest{
-				Messages: make([]types.Message, 0, 4), // Pre-allocate capacity for 4 messages
-			}
-		},
-	}
-
-	embeddingsRequestPool = sync.Pool{
-		New: func() any {
-			return &types.EmbeddingsRequest{
-				Input: make([]string, 0, 2), // Pre-allocate capacity for 2 inputs
-			}
-		},
-	}
-
-	imageRequestPool = sync.Pool{
-		New: func() any {
-			return &types.ImageRequest{}
-		},
-	}
-)
-
-// getTextRequest gets a TextRequest from the pool
-func getTextRequest() *types.TextRequest {
-	req := textRequestPool.Get().(*types.TextRequest)
-	// Reset the request but keep the underlying slice capacity
-	req.Model = ""
-	req.Messages = req.Messages[:0]
-	req.SystemPrompt = ""
-	req.Temperature = nil
-	req.MaxTokens = nil
-	req.TopP = nil
-	req.Stop = nil
-	req.Tools = nil
-	req.ToolChoice = nil
-	req.ResponseFormat = nil
-	req.ProviderOptions = nil
-	return req
-}
-
-// getStructuredRequest gets a StructuredRequest from the pool
-func getStructuredRequest() *types.StructuredRequest {
-	req := structuredRequestPool.Get().(*types.StructuredRequest)
-	// Reset the request but keep the underlying slice capacity
-	req.Model = ""
-	req.Messages = req.Messages[:0]
-	req.SystemPrompt = ""
-	req.Temperature = nil
-	req.MaxTokens = nil
-	req.TopP = nil
-	req.Stop = nil
-	req.Schema = nil
-	req.SchemaName = ""
-	req.ProviderOptions = nil
-	return req
+var embeddingsRequestPool = sync.Pool{
+	New: func() any {
+		return &types.EmbeddingsRequest{
+			Input: make([]string, 0, 2),
+		}
+	},
 }
 
 // getEmbeddingsRequest gets an EmbeddingsRequest from the pool
@@ -91,19 +31,4 @@ func putEmbeddingsRequest(req *types.EmbeddingsRequest) {
 	if req != nil {
 		embeddingsRequestPool.Put(req)
 	}
-}
-
-// getImageRequest gets an ImageRequest from the pool
-func getImageRequest() *types.ImageRequest {
-	req := imageRequestPool.Get().(*types.ImageRequest)
-	// Reset the request
-	req.Prompt = ""
-	req.Model = ""
-	req.N = 0
-	req.Size = ""
-	req.Quality = ""
-	req.Style = ""
-	req.ResponseFormat = ""
-	req.ProviderOptions = nil
-	return req
 }
